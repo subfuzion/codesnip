82c3bf42a3367b5fddd7f8a0f3f69070a09b7c59
Tony Pujals <subfuzion@users.noreply.github.com>
2024-09-13 16:46:51 -0700

    update docs, refactor snippets

--- go/range-over-args.go
+++ go/range-over-args.go
@@ -9,7 +9,7 @@ func main() {
 
 	// Take a slice of args starting at index 1
 	// to skip the program name.
-	args := os.Args[1:]
+	var args = os.Args[1:]
 
 	// Print each arg on its own line.
 	for i, arg := range args {
