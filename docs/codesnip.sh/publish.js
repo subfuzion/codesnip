28fe209d18d36eb381853098122626f75ff2b169
Tony Pujals <subfuzion@users.noreply.github.com>
2024-09-13 16:46:51 -0700

    add analytics

---
+++ docs/codesnip.sh/publish.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZEP72KBRTR"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ZEP72KBRTR');
+</script>
